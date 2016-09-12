8a4f2c65a3715cd78be22ac38f0861672260425d
Roberto Cortez <radcortez@yahoo.com>
2016-09-12 01:47:24 +0100

    Added sample JAX-RS UriInfo.

---
+++ src/main/java/com/radcortez/javaee/java8/jaxrs/uriinfo/UriInfoBean.java
@@ -0,0 +1,72 @@
+package com.radcortez.javaee.java8.jaxrs.uriinfo;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+/**
+ * @author Roberto Cortez
+ */
+@ApplicationPath("/resources")
+@Path("uriinfo")
+@Consumes(APPLICATION_JSON)
+@Produces(APPLICATION_JSON)
+public class UriInfoBean extends Application {
+    private final List<Address> addresses = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        addresses.add(new Address("Ninja Road", "Konoha", "11111", "Land of Fire"));
+        addresses.add(new Address("Windy Road", "Sunagakure", "22222", "Land of Wind"));
+        addresses.add(new Address("Missing", "Unknown", "33333", "Mistery"));
+    }
+
+    @GET
+    @Path("/find")
+    public Address findAddress(@Context final UriInfo uriInfo) {
+        final String street = uriInfo.getQueryParameters().getFirst("street");
+        final String city = uriInfo.getQueryParameters().getFirst("city");
+        final String postalCode = uriInfo.getQueryParameters().getFirst("postalCode");
+        final String country = uriInfo.getQueryParameters().getFirst("country");
+
+        for (Address address : addresses) {
+            if (address.getStreet().equals(street != null ? street : address.getStreet()) &&
+                address.getCity().equals(city != null ? city : address.getCity()) &&
+                address.getPostalCode().equals(postalCode != null ? postalCode : address.getPostalCode()) &&
+                address.getCountry().equals(country != null ? country : address.getCountry())) {
+                return address;
+            }
+        }
+
+        throw new WebApplicationException();
+    }
+
+    @GET
+    @Path("/enhanced")
+    public Address findAddressEnhanced(@Context final UriInfo uriInfo) {
+        final Optional<String> street = Optional.ofNullable(uriInfo.getQueryParameters().getFirst("street"));
+        final Optional<String> city = Optional.ofNullable(uriInfo.getQueryParameters().getFirst("city"));
+        final Optional<String> postalCode = Optional.ofNullable(uriInfo.getQueryParameters().getFirst("postalCode"));
+        final Optional<String> country = Optional.ofNullable(uriInfo.getQueryParameters().getFirst("country"));
+
+        return addresses.stream()
+                        .filter(address -> address.getStreet().equals(street.orElse(address.getStreet())))
+                        .filter(address -> address.getCity().equals(city.orElse(address.getCity())))
+                        .filter(address -> address.getPostalCode().equals(postalCode.orElse(address.getPostalCode())))
+                        .filter(address -> address.getCountry().equals(country.orElse(address.getCountry())))
+                        .findFirst()
+                        .orElseThrow(WebApplicationException::new);
+    }
+}
---
+++ src/test/java/com/radcortez/javaee/java8/jaxrs/uriinfo/UriInfoTest.java
@@ -0,0 +1,67 @@
+package com.radcortez.javaee.java8.jaxrs.uriinfo;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import java.net.URI;
+import java.net.URL;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Roberto Cortez
+ */
+@RunWith(Arquillian.class)
+public class UriInfoTest {
+    @Deployment
+    public static WebArchive createDeployment() {
+        final WebArchive war = ShrinkWrap.create(WebArchive.class)
+                                         .addPackage("com.radcortez.javaee.java8.jaxrs.uriinfo")
+                                         .addAsWebInfResource("beans.xml");
+        System.out.println(war.toString(true));
+        return war;
+    }
+
+    @ArquillianResource
+    private URL base;
+
+    @Test
+    public void testFindAddress() throws Exception {
+        System.out.println("base = " + base);
+
+        final Client client = ClientBuilder.newClient();
+        final Address address = client.target(URI.create(new URL(base, "resources/uriinfo/find").toExternalForm()))
+                                      .queryParam("street", "Ninja Road")
+                                      .queryParam("city", "Konoha")
+                                      .queryParam("postalCode", "11111")
+                                      .queryParam("country", "Land of Fire")
+                                      .request(APPLICATION_JSON)
+                                      .get(Address.class);
+
+        assertNotNull(address);
+    }
+
+    @Test
+    public void testFindAddressEnhanced() throws Exception {
+        System.out.println("base = " + base);
+
+        final Client client = ClientBuilder.newClient();
+        final Address address = client.target(URI.create(new URL(base, "resources/uriinfo/enhanced").toExternalForm()))
+                                      .queryParam("street", "Ninja Road")
+                                      .queryParam("city", "Konoha")
+                                      .queryParam("postalCode", "11111")
+                                      .queryParam("country", "Land of Fire")
+                                      .request(APPLICATION_JSON)
+                                      .get(Address.class);
+
+        assertNotNull(address);
+    }
+}
