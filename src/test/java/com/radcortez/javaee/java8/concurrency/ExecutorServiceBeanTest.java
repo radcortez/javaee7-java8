6c314f5e43b76005c5580474a52dda6866113ce8
Roberto Cortez <radcortez@yahoo.com>
2016-09-13 22:01:12 +0100

    Added ExecutorServices with Java 8 example.

---
+++ src/test/java/com/radcortez/javaee/java8/concurrency/ExecutorServiceBeanTest.java
@@ -0,0 +1,43 @@
+package com.radcortez.javaee.java8.concurrency;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ejb.EJB;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.*;
+
+/**
+ * @author Roberto Cortez
+ */
+@RunWith(Arquillian.class)
+public class ExecutorServiceBeanTest {
+    @Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class)
+                         .addPackage("com.radcortez.javaee.java8.concurrency")
+                         .addAsWebInfResource("beans.xml");
+    }
+
+    @EJB
+    private ManagedExecutorServiceBean managedExecutorServiceBean;
+    @EJB
+    private ManagedScheduledExecutorServiceBean managedScheduledExecutorServiceBean;
+
+    @Test
+    public void testExecuteTask() throws Exception {
+        managedExecutorServiceBean.executeTask();
+    }
+
+    @Test
+    public void testExecuteScheduledTask() throws Exception {
+        managedScheduledExecutorServiceBean.executeTask();
+        SECONDS.sleep(15);
+    }
+}
