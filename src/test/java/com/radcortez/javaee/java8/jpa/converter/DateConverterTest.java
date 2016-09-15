f850730a70300a0e9009bc4d8ea477a53448e8b2
Roberto Cortez <radcortez@yahoo.com>
2016-09-15 14:51:59 +0100

    Added sample with old style Calendar.

--- src/test/java/com/radcortez/javaee/java8/jpa/converter/DateConverterTest.java
+++ src/test/java/com/radcortez/javaee/java8/jpa/converter/DateConverterTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import javax.inject.Inject;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Calendar;
 
 import static java.time.temporal.ChronoUnit.YEARS;
 import static org.junit.Assert.assertEquals;
@@ -43,5 +44,19 @@ public class DateConverterTest {
 
         long years = person.getBirthDate().until(now, YEARS);
         System.out.println(person.getName() + " is " + years + " old.");
+
+        // The old not so good way
+        final Calendar birthDate = Calendar.getInstance();
+        birthDate.set(1990, 4, 10, 0, 0, 0); // Months start at 0!
+        System.out.println("milis = " + birthDate.get(Calendar.MILLISECOND)); // milis are still there!
+        birthDate.set(Calendar.MILLISECOND, 0);
+        final Calendar current = Calendar.getInstance();
+        int diff = current.get(Calendar.YEAR) - birthDate.get(Calendar.YEAR);
+        if (birthDate.get(Calendar.MONTH) > current.get(Calendar.MONTH) ||
+            (birthDate.get(Calendar.MONTH) == current.get(Calendar.MONTH) &&
+             birthDate.get(Calendar.DAY_OF_MONTH) > current.get(Calendar.DAY_OF_MONTH))) {
+            diff--;
+        }
+        System.out.println(person.getName() + " is " + diff + " old.");
     }
 }
